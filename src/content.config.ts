8719ceb1dfc8c98d461f106a7e5bfbf4fa585d03
Anonymous Participant <ctffazect@gmail.com>
2025-12-15 18:00:16 +0700

    Fix: Adding order field for blog posts

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,6 +8,7 @@ const blog = defineCollection({
       title: z.string(),
       description: z.string(),
       date: z.coerce.date(),
+      order: z.number().optional(),
       image: image().optional(),
       tags: z.array(z.string()).optional(),
       authors: z.array(z.string()).optional(),
@@ -64,6 +65,7 @@ const projects = defineCollection({
       tags: z.array(z.string()),
       image: image(),
       link: z.string().url(),
+      order: z.number().optional(),
     }),
 })
 
